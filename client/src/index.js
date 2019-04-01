1bf0a7cedfd5702006e32610a49b257f11844401
gaurav2048 <gunjankalita836@gmail.com>
2019-04-01 18:51:35 +0530

    Initial boilerplate for redux created

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Resources/css/styles.css';
+import {Provider } from 'react-redux'; 
 import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes';
-  
+import { createStore, applyMiddleware } from 'redux';  
+import promiseMiddleware  from 'redux-promise';
+import reduxThunk from 'redux-thunk'; 
 
-ReactDOM.render(
+
+import Reducer from './Reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore); 
+
+
+ReactDOM.render( 
+<Provider store = {createStoreWithMiddleware(Reducer,
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
 <BrowserRouter>
 <Routes />
 </BrowserRouter>
+</Provider>
 , document.getElementById('root'));
  
 // If you want your app to work offline and load faster, you can change
