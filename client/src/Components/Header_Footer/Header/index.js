99e0c2898bcf981687f00fbc796631370130601e
gaurav2048 <gunjankalita836@gmail.com>
2019-04-01 18:22:49 +0530

    Header footer added

---
+++ client/src/Components/Header_Footer/Header/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+    <header className="bck_b_light">
+            <div className="container">
+                <div className="left">
+                    <div className="logo">
+                            WAVES
+                    </div>
+                </div>
+
+                <div className="right">
+                        <div className="top">
+                            Link
+                        </div>
+                        <div className="bottom">
+                            Link
+                        </div>
+                </div>
+
+            </div>
+    </header>
+    )
+  }
+}
--- client/src/HOC/layout.js
+++ client/src/HOC/layout.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import Header from '../Components/Header_Footer/Header/index';
+import Footer from '../Components/Header_Footer/Footer/index'; 
 
 export default class Layout extends Component {
   render() {
     return (
       <div>
-        Hader
+        <Header/>
         <div className="page_container">
           {this.props.children}
         </div>
-        Footer
+        <Footer/>
       </div>
     )
   }
