b28edc1d2b2ad2a2a056c177db8bc3d30470c5e0
gaurav2048 <gunjankalita836@gmail.com>
2019-04-01 16:59:41 +0530

    Initial commit

---
+++ server/models/User.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt  = require('bcrypt');
+const SALT_I = 10;
+require('dotenv').config();
+
+
+const userSchema = mongoose.Schema({
+    email : {
+        type : String, 
+        require: true,
+        trim : true,
+        unique: 1
+    },
+    password: {
+        type : String,
+        required: true,
+        minlength : 5
+    },
+    name: {
+        required: true, 
+        type:String,
+        maxlength: 100 
+    },
+    lastname: {
+        type: String, 
+        required:true,
+        maxlength:100 
+    },
+    cart:{
+        type:Array,
+        default: []
+    },
+    history:{
+        type:Array,
+        default:[]
+    },
+    role:{
+        type:Number,
+        default: 0
+    },
+    token:{
+        type:String,
+        required: false
+    }
+}); 
+
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+    bcrypt.compare(candidatePassword,this.password,function(err, match){
+        if(err) return callback(err);
+        callback(null, match); 
+    })
+}
+
+userSchema.methods.generateToken = function(callback){
+    var user = this; 
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET);
+
+
+    user.token = token; 
+    user.save(function(err,user){
+        if(err) return callback(err);
+        callback(null, user); 
+    })
+
+}
+
+userSchema.pre('save', function (next) {
+var user = this; 
+   if(user.isModified('password')){
+    bcrypt.genSalt(SALT_I, function(err, salt){
+        if(err) return next(err);
+        bcrypt.hash(user.password,salt, function(err, hash){
+            if(err) return next(err);
+            user.password=hash; 
+            next(); 
+        })
+    })
+   }else {
+       next(); 
+   }
+})
+
+
+userSchema.statics.findByToken = function (token, callback){
+    var user = this; 
+
+    jwt.verify(token, process.env.SECRET, function(err,decode){
+        user.findOne({"_id": decode,"token": token}, function(err, user){
+                if(err) return callback(err);
+                callback(null, user); 
+        })
+    })
+
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = {User};
