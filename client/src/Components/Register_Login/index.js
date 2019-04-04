a0f1fd0a10712c8ce301bc5db5da0b7755ce0c1e
gaurav2048 <gunjankalita836@gmail.com>
2019-04-04 16:26:27 +0530

    login component implemented

--- client/src/Components/Register_Login/index.js
+++ client/src/Components/Register_Login/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import MyButton from './../../utils/Button';
+import Login from './login'; 
 
 
 const RegisterLogin = () => {
@@ -13,15 +14,14 @@ const RegisterLogin = () => {
                             <div className="left">
                                 <h1>New Customers</h1>
                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                                    Rerum maxime non unde. Explicabo necessitatibus, consectetur 
-                                    rem laudantium molestiae nesciunt libero ullam omnis, unde tenetur 
-                                    quasi assumenda voluptatum, laboriosam itaque dolore.</p>
+                                    Rerum maxime non unde.
+                                    </p>
                                 <MyButton type="default"  title="Create An Account" linkTo ="/register" addStyle={{ margin:'10px,0,0,0' }}/>
                             </div>
                             <div className-="right">
                                <h2>Register At Customers</h2> 
                                 <p>If you have an account please login.</p>
-                                LOGIN
+                                <Login/> 
                             </div>
                         </div>
 
