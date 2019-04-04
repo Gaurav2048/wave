3f3dd1421b067ee9cde4aebaf408a9fef48ceb9c
gaurav2048 <gunjankalita836@gmail.com>
2019-04-04 19:22:42 +0530

    Register route added ready for api integration

---
+++ client/src/Components/Register_Login/Register.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react'
+import FormField from '../../utils/Form/FormField'; 
+import {Update, generateData ,isFormValid} from '../../utils/Form/FormAction'; 
+import {connect } from 'react-redux';
+import { loginUser} from '../../actions/user_action';
+
+
+class Register extends Component {
+    state ={
+        formError: false, 
+        formSuccess: '',
+        formdata: {
+            name:{
+                element: 'input',
+                value:'',
+                config:{
+                    name:'name_input',
+                    type:"text",
+                    placeholder:'Enter your name'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false, 
+                validationMessage: ''
+            },
+            last_name:{
+                element: 'input',
+                value:'',
+                config:{
+                    name:'last_name_input',
+                    type:"text",
+                    placeholder:'Enter your last_name'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false, 
+                validationMessage: ''
+            },
+            password:{
+                element: 'input',
+                value:'',
+                config:{
+                    name:'password_input',
+                    type:"text",
+                    placeholder:'Enter your password'
+                },
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false, 
+                validationMessage: ''
+            },
+            email:{
+                element: 'input',
+                value:'',
+                config:{
+                    name:'email_input',
+                    type:"email",
+                    placeholder:'Enter your email'
+                },
+                validation: {
+                    required: true, 
+                    email:true
+                },
+                valid: false,
+                touched: false, 
+                validationMessage: ''
+            },
+            cnfirmPassword:{
+                element: 'input',
+                value:'',
+                config:{
+                    name:'cnfirmPassword_input',
+                    type:"text",
+                    placeholder:'Enter your cnfirmPassword'
+                },
+                validation: {
+                    required: true,
+                    confirm:'password'
+                },
+                valid: false,
+                touched: false, 
+                validationMessage: ''
+            },
+        }
+    }
+
+    updateForm = (element) => {
+        const newFormdata = Update(element, this.state.formdata,'register');
+            this.setState({formError: false, formdata: newFormdata})
+    }
+
+    submitForm =(event) =>{
+        event.preventDefault(); 
+
+        let dataToSubmit = generateData(this.state.formData, 'register'); 
+       
+
+        let   formIsValid = isFormValid(this.state.formData, 'register'); 
+
+        if(formIsValid){
+
+           
+            console.log(dataToSubmit);
+        }else {
+            this.setState({formError: true})
+        }
+
+    }
+
+
+  render() {
+    return (
+      <div className="page_wrapper" >
+            <div className="container">
+                    <div className="register_login_container">
+                            <div className="left">
+                                        <form onSubmit={(event) => this.submitForm(event)}>
+                                                <h2>Personal Information</h2>
+                                                <div className="form_block_two">
+                                                    <div className="block">
+                                                    <FormField  id={'name'}  formdata = {this.state.formdata.name} change ={(element) => this.updateForm(element)}/>
+                                                    </div>
+
+                                                    <div className="block">
+                                                    <FormField  id={'last_name'}  formdata = {this.state.formdata.last_name} change ={(element) => this.updateForm(element)}/>
+                                                    </div>
+                                                </div>
+
+                                                <div>
+                                                <FormField  id={'email'}  formdata = {this.state.formdata.email} change ={(element) => this.updateForm(element)}/>
+                                                </div>  
+
+                                                <h2>VERIFY PASSWORD</h2>
+                                                <div className="form_block_two">
+                                                <div className="block">
+                                                    <FormField  id={'password'}  formdata = {this.state.formdata.password} change ={(element) => this.updateForm(element)}/>
+                                                    </div>
+
+                                                    <div className="block">
+                                                    <FormField  id={'cnfirmPassword'}  formdata = {this.state.formdata.cnfirmPassword} change ={(element) => this.updateForm(element)}/>
+                                                    </div>
+                                                </div>
+
+                                                <button onClick={(event)=> this.submitForm(event)}>REGISTER</button>
+                                        </form> 
+                            </div>
+                    </div>
+            </div>
+      </div>
+    )
+  }
+}
+
+
+export default   connect()(Register); 
--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import Home from './Components/Home';
 import Layout from './HOC/layout';
+import Register from './Components/Register_Login/Register';
 import RegisterLogin from './Components/Register_Login/index'; 
 
 
@@ -11,6 +12,7 @@ const Routes = () =>{
     <Switch>
       <Route path="/" exact component={Home}/>
       <Route path="/register_login" exact component={RegisterLogin}/>
+      <Route path="/register" exact component={Register}/>
 
     </Switch>
     </Layout>
