30ac20d75612fc4b17e104f8ade3c9926aae3541
gaurav2048 <gunjankalita836@gmail.com>
2019-04-11 04:07:54 +0530

    srver change

--- server/server.js
+++ server/server.js
@@ -11,11 +11,15 @@ const async = require('async');
 require('dotenv').config();
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+
+app.use(express.static('client/build'))
+
 
 //init 
 
@@ -437,6 +441,14 @@ app.post('/api/user/updateProfile', auth,(req,res)=>{
             }
     )
 })
+
+
+if(process.env.NODE_ENV === 'production'){
+    const path =require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 3002;
